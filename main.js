b0db5b2e1acc0fff2789fd75f5c64cba443c78d5
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2023-06-26 16:06:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 music1= " ";
 music2= " ";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+scoreLeftWrist = 0;
+
 function preload() {
     music1= loadSound("music.mp3");
     music2 = loadSound("music2.mp3")
@@ -12,8 +19,47 @@ function setup() {
 
     video= createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 400, 400);
+    
+    play_music1 = music1.isPlaying();
+    play_music2 = music2.isPlaying();
+
+    fill('#FF0000');
+    stroke('#FF0000');
+
+    if(scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        music2.stop()
+    }
+
+    if(play_music1 = false) {
+        music1.isPlaying();
+        document.getElementById('songName').innerHTML = "Song Name = " + music1
+    }
+}
+
+function modelLoaded() {
+    console.log('Model is initialised');
+}
+
+function gotPoses (results) {
+if(results.length > 0) {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX + " " + "leftWristY = " + leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX + " " + "rightWristY = " + rightWristY);
+
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+}
 }
